c7fe29a0e6757247d1736b8726f3f0147a5cc26c
Kyosuke Takayama <support@mc.neweb.ne.jp>
2006-11-05 14:37:57 +0000

    focusの機能を自作 (ver0.8.0)

--- chrome/content/oretab/focus.js
+++ chrome/content/oretab/focus.js
@@ -1,112 +1,72 @@
-/////////////////////////////////
-// flst.js
-// Daniel Lindkvist
-// 05-09-09
-/////////////////////////////////
 
-var flst =
-{
-  tabHistory: new Array(),
-  nbrOfTabs: 1,
-  oldTab: null,
+window.addEventListener('load', focusInit, false);
 
-  // init
-  init: function()
-  {
-    gBrowser.mTabContainer.addEventListener('select', flst.onTabClose, true);
-    gBrowser.mTabContainer.addEventListener('select', flst.onTabFocus, true);
-    gBrowser.mTabContainer.addEventListener('mousedown', flst.onMouseDown,true);
-    gBrowser.mTabContainer.addEventListener('click', flst.tabClickHandler,true);
+function focusInit() {
+   gBrowser.mTabContainer.addEventListener('mousedown', oreFocus.mousedownTab,true);
+   gBrowser.mTabContainer.addEventListener('click',  oreFocus.clickTab,true);
+   gBrowser.mTabContainer.addEventListener('select', oreFocus.selectTab, true);
 
-    // set flst_id on first tab
-    var tab = gBrowser.selectedTab;
-    tab.setAttribute('flst_id', new Date().getTime());
-    flst.tabHistory.push(tab.getAttribute('flst_id'));
-  },
+   eval("gBrowser.removeTab ="+gBrowser.removeTab.toString().replace(
+            'this.selectedTab =',
+            'if(oldTab._ore) this.selectedTab = oldTab._ore; else this.selectedTab = '
+            ));
 
-  onTabFocus: function(event)
-  {
-    var curTab = gBrowser.selectedTab;
-    var selectedID = curTab.getAttribute('flst_id');
+   var _org = gBrowser.removeTab;
 
-    if (!selectedID){
-      selectedID = new Date().getTime();
-      curTab.setAttribute('flst_id', selectedID);
-    }
+   gBrowser.removeTab = function(aTab) {
+      oreFocus.remove(aTab.linkedPanel);
 
-    if (flst.oldTab != selectedID) {
-      flst.tabHistory.push(selectedID);
-      flst.oldTab = selectedID;
-      // clean out old entry from flst_tabHistory
-      for (var i=0;i<flst.tabHistory.length-1; ++i) {
-        if (flst.tabHistory[i] == selectedID) {
-          flst.tabHistory.splice(i,1);
-          return;
-        }
+      aTab._ore  = id2tab(oreFocus.history.pop());
+      aTab.owner = null;
+
+      _org.apply(gBrowser, arguments);
+   }
+}
+
+function id2tab(id) {
+   var containerChilds = gBrowser.mTabContainer.childNodes;
+   var newIndex = 0;
+   for(var i = 0; i < containerChilds.length; ++i) {
+      if(containerChilds[i].linkedPanel == id) {
+         newIndex = i;
+         break;
       }
-    }
-  },// end onTabFocus
+   }
+   return containerChilds[newIndex];
+}
 
-  onTabClose: function(event)
-  {
-    var nt;
-    var tabs = gBrowser.mTabContainer.childNodes;
+var oreFocus = {
+   history: new Array(),
+   lastTab: null,
 
-    // sync
-    if(tabs.length > flst.nbrOfTabs)
-      flst.nbrOfTabs = tabs.length;
+   remove: function(del) {
+      for(var i = 0; i < oreFocus.history.length; i++)
+         if(oreFocus.history[i] == del) { oreFocus.history.splice(i, 1); i--; }
+   },
 
-
-    // if a tab was closed
-    if(flst.nbrOfTabs > tabs.length) {
-      flst.nbrOfTabs = tabs.length;
-      // remove current tab from history
-      var ot = flst.tabHistory.pop();
-      //get last selected tab
-      if (flst.tabHistory.length > 0)
-        nt = flst.tabHistory.pop();
-      else
-        return;
-
-      flst.selectTab(nt);
-    }
-  }, // end onTabClose
-
-   selectTab: function(id) {
-      var tabs = gBrowser.mTabContainer.childNodes;
-      var newTab;
-      for (var i=tabs.length-1; i>=0; --i) {
-        newTab = tabs[i];
-        if (newTab.getAttribute('flst_id') == id) {
-          gBrowser.selectedTab = newTab;
-          // make sure first tab is in tabHistory
-          if (flst.tabHistory.length == 0)
-            flst.tabHistory.push(id);
-          return;
-        }
+   selectTab: function(event) {
+      // debug('select');
+      if(gBrowser.mCurrentTab) {
+         var newLink = gBrowser.mCurrentTab.linkedPanel;
+         oreFocus.remove(newLink);
+         oreFocus.history.push(newLink);
       }
    },
 
-  onMouseDown: function(event)
-  {
-    if ((event.target.localName == "tab") && (event.target.linkedPanel.indexOf("panel")==0)) {
-      event.stopPropagation();
-    }
-  },
-
-  tabClickHandler: function(event)
-  {
-    if ((event.button == 0) && (event.target.localName == "tab") && (event.target.linkedPanel.indexOf("panel")==0)) {
-      if(gBrowser.selectedTab == event.target) {
-         flst.selectTab(flst.tabHistory[flst.tabHistory.length-2]);
-        // event.stopPropagation();
-      } else {
-         gBrowser.selectedTab = event.target;
+   clickTab: function(event) {
+      if((event.button == 0) && (event.target.localName == "tab")) {
+         if(oreFocus.lastTab == event.target) {
+            // debug('focus');
+            gBrowser.selectedTab = id2tab(oreFocus.history[oreFocus.history.length-2]);
+         }
       }
-    }
-  }
+   },
 
-};
+   mousedownTab: function(event) {
+      if(event.target.localName == "tab")
+         oreFocus.lastTab = gBrowser.selectedTab;
+   }
+}
 
-window.addEventListener('load', flst.init, false);
+// function debug(str) { dump('> ' + str + "\n"); }
 
