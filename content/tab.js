b4e5d087832bae2022e2fb43ba08317e72a7cef9
Kyosuke Takayama <support@mc.neweb.ne.jp>
2008-06-22 10:19:09 +0000

    more simplize.

--- content/tab.js
+++ content/tab.js
@@ -1,19 +1,18 @@
 
-var __org_arg_whereToOpenLink = whereToOpenLink;
-whereToOpenLink = function( e, ignoreButton, ignoreAlt ) {
-   var result = __org_arg_whereToOpenLink(e, ignoreButton, ignoreAlt);
+// history, paste
+window.addEventListener('load', function ORE_openUILinkInit() {
+   eval("openUILink = " + openUILink.toString().replace('where,', 'oretab_switch(where),'));
+} , false);
 
-   // bookmark - PU_openNodeWithEvent
-   // history  - openUILink
-   if(!/function (:?PU_openNodeWithEvent|openUILink)/.test(arguments.callee.caller)) return result;
+// bookmark, sidebar
+var __org_openNodeIn = PlacesUIUtils.openNodeIn;
+PlacesUIUtils.openNodeIn = function ORE_PU_openNodeIn(aNode, aWhere) {
+   var aWhere = oretab_switch(aWhere);
+   if(/^javascript:/.test(aNode.uri)) aWhere = 'current';
+   __org_openNodeIn.call(PlacesUIUtils, aNode, aWhere);
+}
 
-   switch(result) {
-   case 'tab':
-      return 'current';
-   case 'current':
-      return 'tab';
-   default:
-      return result;
-   }
+function oretab_switch(where) {
+   return (where == 'tab') ? 'current' : (where == 'current') ? 'tab' : where;
 }
 
