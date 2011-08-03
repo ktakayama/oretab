8f490621d0bbcc07d3a8125d74428069da67c197
Kyosuke Takayama <loiseau@gmail.com>
2011-08-03 16:49:15 +0900

    ウィンドウを二枚開いた場合に発生する不具合を修正

--- content/tab.js
+++ content/tab.js
@@ -5,11 +5,14 @@ window.addEventListener('load', function ORE_openUILinkInit() {
 } , false);
 
 // bookmark, sidebar
-var __org_openNodeIn = PlacesUIUtils._openNodeIn;
-PlacesUIUtils._openNodeIn = function ORE_PU_openNodeIn(aNode, aWhere, aWindow) {
-   var aWhere = oretab_switch(aWhere);
-   if(/^javascript:/.test(aNode.uri)) aWhere = 'current';
-   return __org_openNodeIn.call(PlacesUIUtils, aNode, aWhere, aWindow);
+if (typeof PlacesUIUtils.__oretab_bload == 'undefined') {
+   PlacesUIUtils.__oretab_bload = true;
+   var __org_openNodeIn = PlacesUIUtils._openNodeIn;
+   PlacesUIUtils._openNodeIn = function ORE_PU_openNodeIn(aNode, aWhere, aWindow) {
+      var aWhere = oretab_switch(aWhere);
+      if(/^javascript:/.test(aNode.uri)) aWhere = 'current';
+      return __org_openNodeIn.call(PlacesUIUtils, aNode, aWhere, aWindow);
+   }
 }
 
 function oretab_switch(where) {
